b03c634bcd4ded678801dfeeaa8b4b1959f23c05
Azamat Khanbabaev <azamat@bm000al-y56vm0v.wucon.wustl.edu>
2025-10-10 12:23:20 -0500

    Added starting ec2 instance code

--- Home.py
+++ Home.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+from streamlit_autorefresh import st_autorefresh
 
 
 # --- Session state ---
@@ -29,40 +30,39 @@ st.session_state.setdefault("HEALTH_URL", "")
 #st.session_state["API_URL"] = "http://3.141.231.76:8000/"
 st.session_state["API_URL"] = "http://52.14.223.10:8000/"
 
-#st.session_state["LAMBDA_URL"] = "https://stmy7sjnqob7vutqkxm5jespmy0vnbps.lambda-url.us-east-1.on.aws/"   # Function URL or API GW route               # EC2 FastAPI base
+st.session_state["LAMBDA_URL"] = "https://hc5ycktqbvxywpf4f4xhxfvm2e0dpozl.lambda-url.us-east-2.on.aws/"   # Function URL or API GW route               # EC2 FastAPI base
 
-#HEALTH_URL = st.session_state["API_URL"] + "healthz"
+HEALTH_URL = st.session_state["API_URL"] + "healthz"
+#---- session flags ----
+st.session_state.setdefault("ec2_start_triggered", False)
+st.session_state.setdefault("fastapi_ready", False)
 
-# ---- session flags ----
-#st.session_state.setdefault("ec2_start_triggered", False)
-#st.session_state.setdefault("fastapi_ready", False)
+def start_ec2_once():
+    if st.session_state["ec2_start_triggered"]:
+        return
+    try:
+        # fire-and-forget: short timeout, don't block
+        requests.post(st.session_state["LAMBDA_URL"], json={}, timeout=10)
+        st.session_state["ec2_start_triggered"] = True
+    except Exception as e:
+        st.warning(f"Could not call Lambda: {e}")
 
-# def start_ec2_once():
-#     if st.session_state["ec2_start_triggered"]:
-#         return
-#     try:
-#         # fire-and-forget: short timeout, don't block
-#         requests.post(st.session_state["LAMBDA_URL"], json={}, timeout=10)
-#         st.session_state["ec2_start_triggered"] = True
-#     except Exception as e:
-#         st.warning(f"Could not call Lambda: {e}")
+def check_health():
+    try:
+        r = requests.get(HEALTH_URL, timeout=2)
+        st.session_state["fastapi_ready"] = r.ok
+    except Exception:
+        st.session_state["fastapi_ready"] = False
 
-# def check_health():
-#     try:
-#         r = requests.get(HEALTH_URL, timeout=2)
-#         st.session_state["fastapi_ready"] = r.ok
-#     except Exception:
-#         st.session_state["fastapi_ready"] = False
-
-# # 1) Kick off EC2 start the first time the user lands on the app
-# start_ec2_once()
-
-# # 2) Poll readiness
-# check_health()
-# if not st.session_state["fastapi_ready"]:
-#     st.info("Waking up the compute node… this usually takes 1–4 minutes.")
-# else:
-#     st.success("Compute node is ready.")
+# 1) Kick off EC2 start the first time the user lands on the app
+start_ec2_once()
+check_health()
+# 2) Poll readiness
+if not st.session_state["fastapi_ready"]:
+    st.info("Waking up the compute node… this usually takes 1–4 minutes.")
+    st_autorefresh(interval=5000, key="preproc_refresh")
+else:
+    st.success("Compute node is ready.")
 
 st.write("# Welcome to NMF exploration tool!")
 st.markdown("""
