6431565080a3dc1fac5a22fe8147879d8c88f846
Azamat Khanbabaev <azamat@bm000al-y56vm0v.wucon.wustl.edu>
2025-10-13 12:48:13 -0500

    minor css change

--- ui_theme.py
+++ ui_theme.py
@@ -1,34 +1,20 @@
 # ui_theme.py
 import streamlit as st
 
+st.set_page_config(page_title="NMF Tool", layout="wide")
+
 def apply_custom_theme():
-    st.set_page_config(layout="wide")
-
-    st.markdown("""
+    st.set_page_config(
+        page_title="NMF Exploration Tool",
+        layout="wide",
+        initial_sidebar_state="expanded"
+    )
+    st.markdown(
+    """
     <style>
-        /* Sidebar background */
-        [data-testid="stSidebar"] {
-            background-color: #9D00FF;
-        }
-
-        /* Sidebar text (labels, titles, etc.) */
-        [data-testid="stSidebar"] * {
-            color: #FFFFFF !important;
-        }
-
-        /* Main area text */
-        .block-container {
-            color: #000000;
-        }
-
-        /* Sidebar buttons */
-        [data-testid="stSidebar"] button {
-            background-color: #DE811D;
-            color: white !important;
-        }
-        [data-testid="stSidebar"] button:hover {
-            background-color: #6BFF00;
-            color: black !important;
-        }
+    [data-testid="stSidebar"] * {
+        color: #FFFFFF !important;
+    }
     </style>
     """, unsafe_allow_html=True)
+    
