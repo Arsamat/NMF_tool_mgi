7c625b56da9674a028ce4b3dd40f067172f18811
Azamat Khanbabaev <azahanbabaev@gmail.com>
2026-02-18 18:27:36 -0600

    Minor variable change

--- brb_data_pages/extract_counts_frontend.py
+++ brb_data_pages/extract_counts_frontend.py
@@ -67,8 +67,8 @@ def extract_data():
     if not st.session_state["authenticated"]:
         authenticate()
     else:
-        FASTAPI_URL = "http://18.218.84.81:8000/"
-        #FASTAPI_URL = "http://3.141.231.76:8000/"
+        #FASTAPI_URL = "http://18.218.84.81:8000/"
+        FASTAPI_URL = "http://3.141.231.76:8000/"
         st.session_state["LAMBDA_URL"] = "https://hc5ycktqbvxywpf4f4xhxfvm2e0dpozl.lambda-url.us-east-2.on.aws/"   # Function URL or API GW route               # EC2 FastAPI base
         st.session_state["API_URL"] = FASTAPI_URL
         #st.session_state["LAMBDA_URL"] = "xxx"
@@ -260,7 +260,7 @@ def extract_data():
                             st.session_state["metadata_index"] = "SampleName"
                             st.session_state["design_factor"] = "Group"
                             st.session_state["brb_data"] = True
-                            st.session_state["gene_column"] = "Unnamed: 0"
+                            st.session_state["gene_column"] = "Geneid"
 
                             st.session_state["_go_to_main"] = "NMF for Bulk RNA"
                             st.rerun()
